7c1ec95ac3aa7943ccd9c1e1c99a0a34abc8cf48
shivakumar <sk625700@gmail.com>
2021-11-10 23:33:12 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace abstract_class
+{
+    public abstract class shape
+    {
+        public string Color { get; set; }
+        public int Thickness { get; set; }
+
+
+        public abstract void Draw();
+
+    }
+    public class Line : shape
+
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw Line");
+        }
+
+    }
+    
+    public class  Rectangle   :  shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draws Rectangle");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+            shape shape1 = new Line { Color = "red", Thickness = 1 };
+            shape shape2 = new Rectangle { Color = "blue", Thickness = 2 };
+            shape1.Draw();
+            shape2.Draw();
+        }
+    }
+
+}
